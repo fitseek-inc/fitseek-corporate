7ca5a60623fdc415dd91dbd4171fb08a75fd2e0c
ryo2 <ryo2.mo@gmail.com>
2026-01-22 12:31:19 +0900

    feat: favicon設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.scss";
 import { Noto_Sans_JP } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import type { Metadata } from "next";
 
 // フォントの設定
 const notoSansJP = Noto_Sans_JP({
@@ -10,9 +11,19 @@ const notoSansJP = Noto_Sans_JP({
   display: "swap",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "株式会社フィットシーク | Fitseek Inc.",
-  description: "新規サービス開発と社内業務システム構築に強みを持つシステム受託開発会社。",
+  description:
+    "新規サービス開発と社内業務システム構築に強みを持つシステム受託開発会社。",
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: ["/favicon.ico"],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
